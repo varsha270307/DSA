40da930c04fd02b16d85b927f9ff8d7bdf1aa55a
varsha singh <varsha270307@gmail.com>
2026-02-03 14:36:24 +0530

    simple programs

--- firstprogram.py
+++ firstprogram.py
@@ -8,3 +8,18 @@ a=int(input("enter the value of a:"))
 b=int(input("enter the value of b:"))
 sum=a+b
 print("sum is:",sum)
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+difference=a-b
+print("difference is:",difference)
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+product=a*b
+print("product is:",product)
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+quotient=a/b
+print("quotient is:",quotient)
