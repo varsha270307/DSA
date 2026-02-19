3b586a673958b9b62cce0a5fd99cdf3af46e5c91
varsha singh <varsha270307@gmail.com>
2026-02-19 19:18:11 +0530

    count inversion

---
+++ count_inversion.py
@@ -0,0 +1,52 @@
+def merge(arr, temp, left, mid, right):
+    i = left       
+    j = mid + 1    
+    k = left       
+    inv_count = 0
+
+    while i <= mid and j <= right:
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
+            i += 1
+        else:
+            temp[k] = arr[j]
+            inv_count += (mid - i + 1)  
+            j += 1
+        k += 1
+
+    while i <= mid:
+        temp[k] = arr[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        temp[k] = arr[j]
+        j += 1
+        k += 1
+
+    for i in range(left, right + 1):
+        arr[i] = temp[i]
+
+    return inv_count
+
+
+def merge_sort(arr, temp, left, right):
+    inv_count = 0
+    if left < right:
+        mid = (left + right) // 2
+
+        inv_count += merge_sort(arr, temp, left, mid)
+        inv_count += merge_sort(arr, temp, mid + 1, right)
+        inv_count += merge(arr, temp, left, mid, right)
+
+    return inv_count
+
+
+def count_inversions(arr):
+    n = len(arr)
+    temp = [0] * n
+    return merge_sort(arr, temp, 0, n - 1)
+
+
+arr = [1, 20, 6, 4, 5]
+print("Number of inversions:", count_inversions(arr))
